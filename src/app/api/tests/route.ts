0b02b34fffc2479b828418e6ef4b6dffdedb69f5
Anton Steblovskiy <steblovskiyanton@yandex.ru>
2024-10-30 16:49:32 +0300

    update env

--- src/app/api/tests/route.ts
+++ src/app/api/tests/route.ts
@@ -5,7 +5,3 @@ export async function GET(req: NextRequest, res: NextResponse) {
   return NextResponse.json(TestsMock, { status: 200 });
 }
 
-export async function POST(req: NextRequest) {
-  const data = await req.json()
-  return NextResponse.json({data})
-}
